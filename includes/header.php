3d22948351d9fd3b2fee31ac03254444e1cf133c
ZandreKruger <zandre@auroradigital.co.za>
2022-06-09 16:03:54 +0200

    - Commented php error report and added main navigation include to header

--- includes/header.php
+++ includes/header.php
@@ -24,7 +24,8 @@
 <body 
 <?php
 
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1); 
+    //error_reporting(E_ALL);
+    //ini_set('display_errors', 1);
+    include 'includes/main-navigation.php';
 
 ?>
