e25de29b0f5be9277aa15ccc569da4f8df3cc8df
ZandreKruger <zandre@auroradigital.co.za>
2022-06-10 17:36:10 +0200

    - Created purchase stock tab

---
+++ includes/purchase-stock-tab.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="bg-light-gray border-radius-20">
+        <!-- Product Items Description -->
+        <div class="row">
+            <div class="col-sm-12">
+                hello world !
+            </div>
+        </div>
+    </div>
+</div>
