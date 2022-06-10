916b6849555a98a657864527f3dc309559fac1e2
ZandreKruger <zandre@auroradigital.co.za>
2022-06-10 16:18:47 +0200

    - Created order history layout and content

--- orderHistory.php
+++ orderHistory.php
@@ -5,4 +5,183 @@ include 'includes/header.php'
 
 ?>
 
+<!-- Cart Items -->
+<section class="orderHistory cart-items bg-white">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1>Your order history</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="bg-light-gray border-radius-20">
+            <!-- Product Items Description -->
+            <div class="row">
+                <div class="col-sm-12 col-md-6 my-auto">
+                    <h1>#265 - R2100.00</h1>
+                    <h4 class="text-dark-gray">Placed on: 2022-06-09 10:33:13</h4>
+                </div>
+                <div class="col-sm-12 col-md-6 my-auto text-end">
+                    <div class="pill pill-danger">
+                        PENDING
+                    </div>
+                </div>
+            </div>
+            <!-- Product Item -->
+            <div class="row">
+                <div class="col-sm-12 col-xl-2 my-auto">
+                    <img class="img-fluid border-radius-20" src="assets/images/products/t-shirt.jpg" alt="Full patch members T-shirts">
+                </div>
+                <div class="col-sm-12 col-xl-3 my-auto">
+                    <h3>Charter t-shirt with logo</h3>
+                    <h5>CRUSAP 000039</h5>
+                    <h3 class="text-vivid-red">R645.00</h3>
+                </div>
+                <div class="col-sm-12 col-xl-2 my-auto xs-pb-20">
+                    <label class="form-label" for="selectSize">Select size</label>
+                    <select class="form-select" id="selectSize" name="selectSize" aria-label="Default select example">
+                        <option selected disabled>Select Size</option>
+                        <option value="XSmall">XSmall</option>
+                        <option value="Small">Small</option>
+                        <option value="Medium">Medium</option>
+                        <option value="Large">Large</option>
+                        <option value="XLarge">XLarge</option>
+                        <option value="XXLarge">XXLarge</option>
+                    </select>
+                </div>
+                <div class="col-sm-12 col-xl-1 my-auto xs-pb-20">
+                    <label class="form-label" for="inputQTY">QTY</label>
+                    <input type="text" id="inputQTY" name="inputQTY" class="form-control" />
+                </div>
+                <div class="col-sm-12 col-xl-3 my-auto text-center">
+                    <h3 class="text-vivid-red">R1100.00</h3>
+                </div>
+            </div>
+            <!-- Product Item -->
+            <div class="row">
+                <div class="col-sm-12 col-xl-2 my-auto">
+                    <img class="img-fluid border-radius-20" src="assets/images/products/hoodies.jpg" alt="Full patch members Hoodies">
+                </div>
+                <div class="col-sm-12 col-xl-3 my-auto">
+                    <h3>Hoodie</h3>
+                    <h5>CRUSAP 000039</h5>
+                    <h3 class="text-vivid-red">R645.00</h3>
+                </div>
+                <div class="col-sm-12 col-xl-2 my-auto xs-pb-20">
+                    <label class="form-label" for="selectSize">Select size</label>
+                    <select class="form-select" id="selectSize" name="selectSize" aria-label="Default select example">
+                        <option selected disabled>Select Size</option>
+                        <option value="XSmall">XSmall</option>
+                        <option value="Small">Small</option>
+                        <option value="Medium">Medium</option>
+                        <option value="Large">Large</option>
+                        <option value="XLarge">XLarge</option>
+                        <option value="XXLarge">XXLarge</option>
+                    </select>
+                </div>
+                <div class="col-sm-12 col-xl-1 my-auto xs-pb-20">
+                    <label class="form-label" for="inputQTY">QTY</label>
+                    <input type="text" id="inputQTY" name="inputQTY" class="form-control" />
+                </div>
+                <div class="col-sm-12 col-xl-3 my-auto text-center">
+                    <h3 class="text-vivid-red">R1000.00</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="bg-light-gray border-radius-20">
+            <!-- Product Items Description -->
+            <div class="row">
+                <div class="col-sm-12 col-md-6 my-auto">
+                    <h1>#196 - R645.00</h1>
+                    <h4 class="text-dark-gray">Placed on: 2022-06-09 10:33:13</h4>
+                </div>
+                <div class="col-sm-12 col-md-6 my-auto text-end">
+                    <div class="pill pill-success">
+                        DELIVERED
+                    </div>
+                </div>
+            </div>
+            <!-- Product Item -->
+            <div class="row">
+                <div class="col-sm-12 col-xl-2 my-auto">
+                    <img class="img-fluid border-radius-20" src="assets/images/products/t-shirt.jpg" alt="Full patch members T-shirts">
+                </div>
+                <div class="col-sm-12 col-xl-3 my-auto">
+                    <h3>Charter t-shirt with logo</h3>
+                    <h5>CRUSAP 000039</h5>
+                    <h3 class="text-vivid-red">R645.00</h3>
+                </div>
+                <div class="col-sm-12 col-xl-2 my-auto xs-pb-20">
+                    <label class="form-label" for="selectSize">Select size</label>
+                    <select class="form-select" id="selectSize" name="selectSize" aria-label="Default select example">
+                        <option selected disabled>Select Size</option>
+                        <option value="XSmall">XSmall</option>
+                        <option value="Small">Small</option>
+                        <option value="Medium">Medium</option>
+                        <option value="Large">Large</option>
+                        <option value="XLarge">XLarge</option>
+                        <option value="XXLarge">XXLarge</option>
+                    </select>
+                </div>
+                <div class="col-sm-12 col-xl-1 my-auto xs-pb-20">
+                    <label class="form-label" for="inputQTY">QTY</label>
+                    <input type="text" id="inputQTY" name="inputQTY" class="form-control" />
+                </div>
+                <div class="col-sm-12 col-xl-3 my-auto text-center">
+                    <h3 class="text-vivid-red">R1100.00</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="bg-light-gray border-radius-20 opacity-half">
+            <!-- Product Items Description -->
+            <div class="row">
+                <div class="col-sm-12 col-md-6 my-auto">
+                    <h1>#29 - R645.00</h1>
+                    <h4 class="text-dark-gray">Placed on: 2022-06-09 10:33:13</h4>
+                </div>
+                <div class="col-sm-12 col-md-6 my-auto text-end">
+                    <div class="pill pill-dark">
+                        CANCELLED
+                    </div>
+                </div>
+            </div>
+            <!-- Product Item -->
+            <div class="row">
+                <div class="col-sm-12 col-xl-2 my-auto">
+                    <img class="img-fluid border-radius-20" src="assets/images/products/t-shirt.jpg" alt="Full patch members T-shirts">
+                </div>
+                <div class="col-sm-12 col-xl-3 my-auto">
+                    <h3>Charter t-shirt with logo</h3>
+                    <h5>CRUSAP 000039</h5>
+                    <h3 class="text-vivid-red">R645.00</h3>
+                </div>
+                <div class="col-sm-12 col-xl-2 my-auto xs-pb-20">
+                    <label class="form-label" for="selectSize">Select size</label>
+                    <select class="form-select" id="selectSize" name="selectSize" aria-label="Default select example">
+                        <option selected disabled>Select Size</option>
+                        <option value="XSmall">XSmall</option>
+                        <option value="Small">Small</option>
+                        <option value="Medium">Medium</option>
+                        <option value="Large">Large</option>
+                        <option value="XLarge">XLarge</option>
+                        <option value="XXLarge">XXLarge</option>
+                    </select>
+                </div>
+                <div class="col-sm-12 col-xl-1 my-auto xs-pb-20">
+                    <label class="form-label" for="inputQTY">QTY</label>
+                    <input type="text" id="inputQTY" name="inputQTY" class="form-control" />
+                </div>
+                <div class="col-sm-12 col-xl-3 my-auto text-center">
+                    <h3 class="text-vivid-red">R1100.00</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 <?php include 'includes/footer.php' ?>
