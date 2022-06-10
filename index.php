9cfd27af741837e2ee049eddb2263650396f10a2
ZandreKruger <zandre@auroradigital.co.za>
2022-06-10 09:34:59 +0200

    - Created hero banner

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
-<?php 
+<?php
 
 $page_name = "Home";
 include 'includes/header.php'
 
 ?>
 
-<!-- Content goes here -->
-<div class="container">
-    <div class="row">
-        <div class="col-sm-12" data-aos="zoom-out-up" data-aos-duration="600" data-aos-delay="500">
-            <h1>Hello World!</h1>
+<!-- Hero Banner -->
+<section class="hero-banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12" data-aos="zoom-out-up" data-aos-duration="600" data-aos-delay="500">
+                <h1>Welcome to the Crusaders shop</h1>
+                <img class="img-fluid border-radius-25" src="assets/images/crusaders-banner@2x.jpg" alt="Crusaders Banner">
+                <div class="alert alert-danger" role="alert">
+                    To get started add a product to the cart, select your size and quantity. All prices are inclusive of VAT.
+                </div>
+            </div>
         </div>
     </div>
-</div>
+</section>
 
 <?php include 'includes/footer.php' ?>
