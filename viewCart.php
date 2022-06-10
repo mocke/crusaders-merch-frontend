738c97a47dae46a0e58b4c4e8f675e91a97dbde8
ZandreKruger <zandre@auroradigital.co.za>
2022-06-10 14:23:25 +0200

    - Created heroCart hero section

--- viewCart.php
+++ viewCart.php
@@ -5,4 +5,21 @@ include 'includes/header.php'
 
 ?>
 
+<!-- View Cart Hero Banner -->
+<section class="viewCart">
+    <div class="container">
+        <div class="col-sm-12 d-flex">
+            <h1>Confirm your order</h1>
+            <div class="result-pill">
+                3 items
+            </div>
+        </div>
+        <div class="col-sm-12 xs-mt-25">
+            <div class="alert alert-danger" role="alert">
+                Confirm your sizes and quantity before placing and order
+            </div>
+        </div>
+    </div>
+</section>
+
 <?php include 'includes/footer.php' ?>
