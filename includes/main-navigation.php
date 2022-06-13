b01127e0a6b7d2831d2e4b8b873fd74456adde87
ZandreKruger <zandre@auroradigital.co.za>
2022-06-13 12:55:12 +0200

    - Updated main navigation responsiveness

--- includes/main-navigation.php
+++ includes/main-navigation.php
@@ -2,19 +2,19 @@
     <div class="container">
         <div class="col-sm-12">
             <nav class="navbar navbar-expand-lg navbar-dark">
-                <div class="container-fluid">
+                <div class="container-fluid xs-plr-0">
                     <a class="navbar-brand" href="index.php">
                         <img class="img-fluid" src="assets/images/logo/crusaders-badge-logo@2x.png" alt="Crusaders Badge Logo">
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="collapse navbar-collapse text-end text-lg-start" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-lg-0">
-                            <li class="nav-item lg-mr-5">
+                            <li class="nav-item my-auto lg-mr-5">
                                 <a class="nav-link <?php if($page_name == "index"){echo "active";} else {} ?>" href="index.php">Shop</a>
                             </li>
-                            <li class="nav-item">
+                            <li class="nav-item my-auto">
                                 <a class="nav-link <?php if($page_name == "orderHistory"){echo "active";} else {} ?>" href="orderHistory.php">My orders</a>
                             </li>
                         </ul>
@@ -22,7 +22,10 @@
                             <li class="nav-item my-auto">
                                 <a class="nav-link <?php if($page_name == "orderCharters"){echo "active";} else {} ?>" href="orderCharters.php">charter orders</a>
                             </li>
-                            <li class="nav-item lg-ml-35">
+                            <li class="nav-item my-auto d-lg-none">
+                                <a class="nav-link <?php if($page_name == "viewCart"){echo "active";} else {} ?>" href="viewCart.php">cart (3 items)</a>
+                            </li>
+                            <li class="nav-item d-none d-lg-inline-block lg-ml-35">
                                 <a class="btn btn-primary" href="viewCart.php">cart (3 items)</a>
                             </li>
                         </ul>
